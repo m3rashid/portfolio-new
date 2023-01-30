b74f99cef6586ddc4889ae6f66e5eaf2902e3345
MD Rashid Hussain <md2000399@st.jmi.ac.in>
2023-01-30 06:36:25 +0000

    static

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { render, hydrate } from "react-dom";
 import { App } from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
+const rootElement = document.getElementById("root") as HTMLElement;
+// const root = createRoot(rootElement);
 
-root.render(
+const InnerApp = (
   <React.StrictMode>
     <ColorModeScript initialColorMode="dark" />
     <App />
   </React.StrictMode>
-);
+)
+
+if (rootElement.hasChildNodes()) {
+	hydrate(InnerApp, rootElement);
+} else {
+	render(InnerApp, rootElement);
+}
