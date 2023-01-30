bde0ff5fbd396f99cbf2a863c26c79383dfb98b4
MD Rashid Hussain <m3rashid.hussain@gmail.com>
2023-01-30 15:44:02 +0530

    optimized for production

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ yarn react-scripts build
 yarn react-snap 
 
 cd build && sed -i 's/http:\/\/localhost:45678//g' $(find . -type f)
+
+echo "Build Succeeded, deploying site to netlify..."
+sleep 2
+
+ntl deploy --prod
